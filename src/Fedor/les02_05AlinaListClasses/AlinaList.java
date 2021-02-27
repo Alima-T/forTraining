c7ba1d36c4fcb6522e1910c6e4919be06450d72c
Alina <K.alinka555@list.ru>
2021-02-28 00:55:35 +0300

    Documentation written

--- src/Fedor/les02_05AlinaListClasses/AlinaList.java
+++ src/Fedor/les02_05AlinaListClasses/AlinaList.java
@@ -8,27 +8,13 @@ import static org.junit.Assert.assertTrue;
  */
 public class AlinaList implements IList {
 
-    /*Если хочешь задачу с классом, то вот тебе задача. Создай класс, который позволяет хранить список long во внутреннем массиве long[],
-    при этом у него есть методы void add(long value), long get(int index), int size(), int indexOf(long value), void clear()
-    если размера внутреннего массива перестает хватать, надо заменить его бОльшим массивом и скопировать в него все старые значения
-    Класс назови AlinaList
-            да, массив
-    я хочу, чтобы ты создала класс, внутри которого есть массив, и все манипуляции с этим массивом выполняла через методы класса
-    + void add(long value) - добавляет в список текущего экземпляра (объекта) новое значение value
-    + long get(int index) - возвращает значение из списка текущего экземпляра с позицией index
-    int size() - возвращает количество элементов, сохраненных в списке текущего экземпляра
-    int indexOf(long value) - возвращает позицию указанного значения в списке текущего экземпляра
-    void clear() - опустошает список текущего экземпляра
-    + indexOf - ты делала вчера только*/
-
     long[] list = new long[0];
 
     @Override
-//    public void add(long value) {
-//        list = new long[list.length + 1];
-//        list[list.length - 1] = value;
-
-//    }
+    /**
+     * Добавляет новое значение в список
+     * @param value добавляемое в список значение
+     */
     public void add(long value) { // + void add(long value) - добавляет в список текущего экземпляра (объекта) новое значение value
         long[] newList = new long[list.length + 1]; //
         for (int i = 0; i < list.length; i = i + 1) {
@@ -39,22 +25,34 @@ public class AlinaList implements IList {
     }
 
     @Override
+    /**
+     * Предоставляет значение из списка по индексу
+     * @param index - индекс получаемого элемента списка
+     * @return значение элемента с указанным индексом
+     */
     public long get(int index) {
+        long element = list[index];
         for (int i = 0; i < list.length; i++) {
-            if (index == list[i]) {
+            if (index == list[i])
                 return i;
-            }
         }
-        return 0;
+        return element;
     }
 
     @Override
+    /**
+     * Предоставляет размер списка
+     * @return количество элементов в списке
+     */
     public int size() {
         int size = list.length;
         return size;
     }
 
     @Override
+    /**
+     * Очищает список
+     */
     public void clear() {
         for (long array : list) {
             list = new long[0];
@@ -62,6 +60,11 @@ public class AlinaList implements IList {
     }
 
     @Override
+    /**
+     * Предоставляет индекс первого элемента списка по указанному значению
+     * @param value - значение элемента списка
+     * @return - индекс первого найденного элемента с указанным значением
+     */
     public int indexOf(long value) {
         for (int i = 0; i < list.length; i++) {
             if (list[i] == value) {
@@ -70,6 +73,7 @@ public class AlinaList implements IList {
         }
         return -1;
     }
+
 
     // TDD - Test Driven Development
     public static void main(String[] args) {
@@ -97,6 +101,5 @@ public class AlinaList implements IList {
         } catch (IndexOutOfBoundsException ex) {
         }
     }
-
 }
 
