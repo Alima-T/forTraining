57403ce8709f269441629844c91e08bcdb7ba105
Alina <K.alinka555@list.ru>
2021-02-28 18:38:29 +0300

    ProductSearchByNumberMethod corrected

---
+++ src/Fedor/les02_26Conlsole/tests/ProductSearchByNumberMethod.java
@@ -0,0 +1,31 @@
+package Fedor.les02_26Conlsole.tests;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * программа получает название файла с реестром и порядковый номер товара (который с 1, а не с 0), после чего печатает его
+ */
+public class ProductSearchByNumberMethod {
+
+    public void productSearchByNumber(Path registerPath, int index) throws IOException {
+        final String PRODUCT_DELIMITER = "#";
+        final String CHARSET = "cp1251";
+        final Charset REGISTER_CHARSET = Charset.forName(CHARSET);
+
+        Path pathOfRegister = Path.of(String.valueOf(registerPath));
+        String register = Files.readString(pathOfRegister, REGISTER_CHARSET);
+        String[] productArray = register.split(PRODUCT_DELIMITER);
+
+        for (String element : productArray) {
+        }
+        System.out.println(productArray[index]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ProductSearchByNumberMethod productByNumber = new ProductSearchByNumberMethod();
+        productByNumber.productSearchByNumber(Path.of("C:\\Users\\Alina\\Desktop\\register.txt"), 4);
+    }
+}
